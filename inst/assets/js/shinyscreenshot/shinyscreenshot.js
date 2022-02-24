14df55ba482da19fd68345ca29a78dd910f5e39c
mapmakr <100140516+mapmakr@users.noreply.github.com>
2022-02-24 07:43:55 -0500

    Update shinyscreenshot.js

--- inst/assets/js/shinyscreenshot/shinyscreenshot.js
+++ inst/assets/js/shinyscreenshot/shinyscreenshot.js
@@ -14,7 +14,7 @@ var shinyscreenshot = {
         useCORS : true,
         // height : window.outerHeight * window.innerHeight,
         // width : window.outerWidth * window.innerWidth
-        height : document.documentElement.clientHeight,
+        height : document.documentElement.clientHeight-112,
         width : document.documentElement.clientWidth
       }
     ).then(function(canvas) {
